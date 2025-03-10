90964a404edbb079d7de332a11ab97c4f5643d67
Kasper Fabricius Kristensen <45367945+kasperkristensen@users.noreply.github.com>
2025-03-10 12:31:19 +0100

    fix(medusa): Apply middleware to GET customers/:id/addresses/:address_id (#11788)

---
+++ .changeset/gorgeous-rats-watch.md
@@ -0,0 +1,5 @@
+---
+"@medusajs/medusa": patch
+---
+
+fix(medusa): Apply middleware to GET customers/:id/addresses/:address_id
--- packages/medusa/src/api/admin/customers/middlewares.ts
+++ packages/medusa/src/api/admin/customers/middlewares.ts
@@ -4,17 +4,18 @@ import {
   AdminCreateCustomer,
   AdminCreateCustomerAddress,
   AdminCustomerAddressesParams,
+  AdminCustomerAddressParams,
   AdminCustomerParams,
   AdminCustomersParams,
   AdminUpdateCustomer,
   AdminUpdateCustomerAddress,
 } from "./validators"
 
-import { MiddlewareRoute } from "@medusajs/framework/http"
 import {
   validateAndTransformBody,
   validateAndTransformQuery,
 } from "@medusajs/framework"
+import { MiddlewareRoute } from "@medusajs/framework/http"
 import { createLinkBody } from "../../utils/validators"
 
 export const adminCustomerRoutesMiddlewares: MiddlewareRoute[] = [
@@ -67,6 +68,16 @@ export const adminCustomerRoutesMiddlewares: MiddlewareRoute[] = [
       validateAndTransformBody(AdminCreateCustomerAddress),
       validateAndTransformQuery(
         AdminCustomerParams,
+        QueryConfig.retrieveTransformQueryConfig
+      ),
+    ],
+  },
+  {
+    method: ["GET"],
+    matcher: "/admin/customers/:id/addresses/:address_id",
+    middlewares: [
+      validateAndTransformQuery(
+        AdminCustomerAddressParams,
         QueryConfig.retrieveTransformQueryConfig
       ),
     ],
