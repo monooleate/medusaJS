18dc3cf9c9e748eec78b516b32e9f0ceea1d4640
Shahed Nasser <shahednasser@gmail.com>
2025-04-11 11:58:19 +0300

    docs: changes for next release (#12098)
    * docs: changes for next release
    
    * generate
    
    * generate

--- www/apps/book/generated/edit-dates.mjs
+++ www/apps/book/generated/edit-dates.mjs
@@ -101,7 +101,7 @@ export const generatedEditDates = {
   "app/learn/fundamentals/workflows/multiple-step-usage/page.mdx": "2024-11-25T16:19:32.169Z",
   "app/learn/installation/page.mdx": "2025-03-11T08:55:12.967Z",
   "app/learn/fundamentals/data-models/check-constraints/page.mdx": "2024-12-06T14:34:50.384Z",
-  "app/learn/fundamentals/module-links/link/page.mdx": "2025-01-06T09:27:25.604Z",
+  "app/learn/fundamentals/module-links/link/page.mdx": "2025-04-07T08:03:14.513Z",
   "app/learn/fundamentals/workflows/store-executions/page.mdx": "2025-02-11T15:56:03.835Z",
   "app/learn/fundamentals/plugins/create/page.mdx": "2025-04-07T14:50:53.866Z",
   "app/learn/fundamentals/plugins/page.mdx": "2025-01-22T10:14:10.433Z",
