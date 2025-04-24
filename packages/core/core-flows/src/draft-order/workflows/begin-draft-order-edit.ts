71a5dc448db53d93503975caf9aa949068e8281f
Shahed Nasser <shahednasser@gmail.com>
2025-04-24 11:31:28 +0300

    chore(core-flows, types): add tsdocs for draft orders workflows (#12279)

--- packages/core/core-flows/src/draft-order/workflows/begin-draft-order-edit.ts
+++ packages/core/core-flows/src/draft-order/workflows/begin-draft-order-edit.ts
@@ -11,6 +11,27 @@ import { validateDraftOrderStep } from "../steps"
 
 export const beginDraftOrderEditWorkflowId = "begin-draft-order-edit"
 
+/**
+ * This workflow begins a draft order edit. It's used by the
+ * [Create Draft Order Edit Admin API Route](https://docs.medusajs.com/api/admin#draft-orders_postdraftordersidedit).
+ * 
+ * The draft order edit can later be requested using {@link requestDraftOrderEditWorkflow} or confirmed using {@link confirmDraftOrderEditWorkflow}.
+ * 
+ * You can use this workflow within your customizations or your own custom workflows, allowing you to wrap custom logic around 
+ * creating a draft order edit request.
+ * 
+ * @example
+ * const { result } = await beginDraftOrderEditWorkflow(container)
+ * .run({
+ *   input: {
+ *     order_id: "order_123",
+ *   }
+ * })
+ * 
+ * @summary
+ * 
+ * Create a draft order edit request.
+ */
 export const beginDraftOrderEditWorkflow = createWorkflow(
   beginDraftOrderEditWorkflowId,
   function (input: WorkflowData<OrderWorkflow.BeginorderEditWorkflowInput>) {
--- packages/core/core-flows/src/draft-order/workflows/remove-draft-order-shipping-method.ts
+++ packages/core/core-flows/src/draft-order/workflows/remove-draft-order-shipping-method.ts
@@ -24,9 +24,12 @@ import { refreshDraftOrderAdjustmentsWorkflow } from "./refresh-draft-order-adju
 export const removeDraftOrderShippingMethodWorkflowId =
   "remove-draft-order-shipping-method"
 
-interface RemoveDraftOrderShippingMethodWorkflowInput {
+/**
+ * The details of the shipping method to remove from a draft order.
+ */
+export interface RemoveDraftOrderShippingMethodWorkflowInput {
   /**
-   * The ID of the draft order to add the shipping methods to.
+   * The ID of the draft order to remove the shipping method from.
    */
   order_id: string
   /**
@@ -35,6 +38,26 @@ interface RemoveDraftOrderShippingMethodWorkflowInput {
   shipping_method_id: string
 }
 
+/**
+ * This workflow removes an existing shipping method from a draft order edit. It's used by the
+ * [Remove Shipping Method from Draft Order Edit Admin API Route](https://docs.medusajs.com/api/admin#draft-orders_deletedraftordersideditshippingmethodsmethodmethod_id).
+ * 
+ * You can use this workflow within your customizations or your own custom workflows, allowing you to wrap custom logic around
+ * removing a shipping method from a draft order edit.
+ * 
+ * @example
+ * const { result } = await removeDraftOrderShippingMethodWorkflow(container)
+ * .run({
+ *   input: {
+ *     order_id: "order_123",
+ *     shipping_method_id: "sm_123",
+ *   }
+ * })
+ * 
+ * @summary
+ * 
+ * Remove an existing shipping method from a draft order edit.
+ */
 export const removeDraftOrderShippingMethodWorkflow = createWorkflow(
   removeDraftOrderShippingMethodWorkflowId,
   function (input: WorkflowData<RemoveDraftOrderShippingMethodWorkflowInput>) {
