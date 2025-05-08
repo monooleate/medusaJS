c2914f2ebd072a29b4c1d74a93d0cdd221c4d0e7
Shahed Nasser <shahednasser@gmail.com>
2025-05-08 18:01:45 +0300

    chore: update Medusa UI packages in docs on new release (#12415)

--- www/apps/ui/scripts/copy-colors.ts
+++ www/apps/ui/scripts/copy-colors.ts
@@ -1,4 +1,5 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-require-imports */
+
 /* eslint-disable no-console */
 const fs = require("fs")
 const path = require("path")
