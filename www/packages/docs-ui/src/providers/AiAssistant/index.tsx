be7a5b802fd208ebb40e64c05339087f30dd67f5
Shahed Nasser <shahednasser@gmail.com>
2025-07-14 10:05:55 +0300

    docs: add missing prop to kapa (#12934)
    * docs: add missing prop to kapa
    
    * change to cookie

--- www/packages/docs-ui/src/providers/AiAssistant/index.tsx
+++ www/packages/docs-ui/src/providers/AiAssistant/index.tsx
@@ -185,6 +185,7 @@ export const AiAssistantProvider = ({
           onQuerySubmit: () => setPreventAutoScroll(false),
         },
       }}
+      userTrackingMode="cookie"
     >
       <AiAssistantInnerProvider
         {...props}
